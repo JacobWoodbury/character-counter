1c6faf603a6980044d883a8a8611e1ba96a5adfa
Jacob Woodbury <166659344+JacobWoodbury@users.noreply.github.com>
2025-01-16 12:03:43 -0800

    added first test

--- src/CharacterFrequencyCounterTest.java
+++ src/CharacterFrequencyCounterTest.java
@@ -3,5 +3,36 @@ import static org.junit.Assert.assertEquals;
 import org.junit.jupiter.api.Test;
 
 public class CharacterFrequencyCounterTest {
+
+    @Test
+    public void testLAppearsThreeTimesInHellowWorld(){
+        //Arrange
+        CharacterFrequencyCounter counter = new CharacterFrequencyCounter("hello world");
+
+        //Act
+        int actualCount = counter.getFrequency('l');
+
+        //Assert
+        assertEquals(3, actualCount);
+    }
+
+    @Test
+    public void testUpperAndLowerCase(){
+        CharacterFrequencyCounter counter = new CharacterFrequencyCounter("AadbaA");
+
+        int actualCount = counter.getFrequency('A');
+
+        assertEquals(4, actualCount);
+
+    }
+
+    @Test
+    public void testPercentZeroForNonOccuringChar(){
+        CharacterFrequencyCounter counter = new CharacterFrequencyCounter("hi there");
+
+        double actualPercent = counter.getRelativePercentage('z');
+
+        assertEquals(0.0, actualPercent, 0.0001);
+    }
     
 }
